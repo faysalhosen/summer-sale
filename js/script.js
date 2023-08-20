3860d00961ed76842add3d5ca3bea9db77a9ccaa
faysalhosen <faysanhosen7025@gmail.com>
2023-08-20 20:33:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// document.getElementById('kitchenBtn').addEventListener('click',function(){
+//     console.log('kitchenBtn button clicked');
+// })
+
+
+ function handleClickBtn(target){
+  console.log(target.patentNode);
+//    const selectedItemsContainer = document.getElementById ("selected-Items")
+//   const itemName = target.parentNode.childrenNode[1].innerText;
+//   li.innerText = itemName;
+//   selectedItemsContainer.appendChild(li);
+// console.log(target.parentNode.childrenNode[2].innerText)
+ }
+
