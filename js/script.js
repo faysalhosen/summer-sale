c2b6828f77cdfd0c922e39928ece0188c9a808ad
faysalhosen <faysanhosen7025@gmail.com>
2023-08-21 14:22:57 +0600

    modify

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-// document.getElementById('kitchenBtn').addEventListener('click',function(){
-//     console.log("hello world");
-// })
- function handleClickBtn(event){
-  const productName = event.childNodes[3].childNodes[3].innerText;
-  // console.log(productName);
-  const productNameContainer = document.querySelector("#selected-Items")
-  const count = productNameContainer.childElementCount;
- const p=document.createElement("p");
- p.innerText= count+1+" "+productName;
- productNameContainer.appendChild(p);
 
-  // console.log(event.childNodes[3].childNodes[3].innerText);
+//  function handleClickBtn(event){
+//   const productName = event.childNodes[3].childNodes[3].innerText;
+//   // console.log(productName);
+//   const productNameContainer = document.querySelector("#selected-Items")
+//   const count = productNameContainer.childElementCount;
+//  const p=document.createElement("p");
+//  p.innerText= count+1+" "+productName;
+//  productNameContainer.appendChild(p);
 
-//    const selectedItemsContainer = document.getElementById ("selected-Items")
-//   const itemName = target.parentNode.childrenNode[1].innerText;
-//   li.innerText = itemName;
-//   selectedItemsContainer.appendChild(li);
-// console.log(target.parentNode.childrenNode[2].innerText)
-//  }
+//  const productPrice = event.childNodes[3].childNodes[3].innerText;
+// console.log(productPrice);
+
+let total = 0;
+function handleClickShowBtn(areaType) {
+    const areaText = document.getElementById('text-area');
+    const count = areaText.childElementCount;
+    const itemName = areaType.childNodes[3].childNodes[3].innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-area-desing')
+    p.innerHTML = ${count + 1}. ${itemName};
+    areaText.appendChild(p);
+
+    // price total 
+    const price = areaType.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    total = parseFloat(total) + parseFloat(price);
+    console.log(total);
+
+    document.getElementById('total-price').innerText = total;
+    getAplyInputField();
+    setFinalTotal();
+
+}d
+  
+ }
 
  
-
-// function handleClickBtn(){
-//   console.log("hello world")
- }
