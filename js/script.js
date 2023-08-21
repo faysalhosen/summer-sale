f6c8e86e5d6a773689afe3ee8bb17eed14d9d647
faysalhosen <faysanhosen7025@gmail.com>
2023-08-21 09:49:28 +0600

    added the javascipt area

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 // document.getElementById('kitchenBtn').addEventListener('click',function(){
-//     console.log('kitchenBtn button clicked');
+//     console.log("hello world");
 // })
+ function handleClickBtn(event){
+  const productName = event.childNodes[3].childNodes[3].innerText;
+  // console.log(productName);
+  const productNameContainer = document.querySelector("#selected-Items")
+  const count = productNameContainer.childElementCount;
+ const p=document.createElement("p");
+ p.innerText= count+1+" "+productName;
+ productNameContainer.appendChild(p);
 
+  // console.log(event.childNodes[3].childNodes[3].innerText);
 
- function handleClickBtn(target){
-  console.log(target.patentNode);
 //    const selectedItemsContainer = document.getElementById ("selected-Items")
 //   const itemName = target.parentNode.childrenNode[1].innerText;
 //   li.innerText = itemName;
 //   selectedItemsContainer.appendChild(li);
 // console.log(target.parentNode.childrenNode[2].innerText)
- }
+//  }
 
+ 
+
+// function handleClickBtn(){
+//   console.log("hello world")
+ }
